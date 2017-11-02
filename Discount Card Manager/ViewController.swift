724b1f0939f0c5815bbdc732455cf5bcbad2e333
nazarii.melnyk <nazarij.melnyk@gmail.com>
2017-11-02 14:22:25 +0200

    Initial Commit

---
+++ Discount Card Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Discount Card Manager
+//
+//  Created by Nazarii Melnyk on 11/2/17.
+//  Copyright © 2017 Nazarii Melnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
